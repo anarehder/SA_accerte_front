7109b175eeb14dc38d97071bbd6b94aec5fa27d4
anarehder <accerte@controledeponto.subnetmonitorpr.vpnproducao.oraclevcn.com>
2024-03-19 13:33:06 +0000

    feat: arquitetura inicial e homepage base

---
+++ src/pages/HomePage.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export default function HomePage() {
+
+    return(
+        <PageContainer>
+            MINHA HOMEPAGE
+        </PageContainer>
+    )
+}
+
+const PageContainer = styled.div`
+    min-height: 100vh;
+    background-color: black;
+    color: white;
+`
